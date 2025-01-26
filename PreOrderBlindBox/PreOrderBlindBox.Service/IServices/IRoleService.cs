a557405bdb98e526ff35b115a7e4beb9bac1cc9c
NguyenDHung <hungndse171325@fpt.edu.vn>
2025-01-26 15:59:38 +0700

    Test merge github

--- PreOrderBlindBox/PreOrderBlindBox.Service/IServices/IRoleService.cs
+++ PreOrderBlindBox/PreOrderBlindBox.Service/IServices/IRoleService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PreOrderBlindBox.Data.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace PreOrderBlindBox.Services.IServices
 {
-    public interface IRoleService
-    {
-    }
+	public interface IRoleService
+	{
+		public Task<Role> GetRoleByIdAsync(int roleId);
+		public void CreateRoleAsync(string roleName);
+		public void DeleteRoleByIdAsync(int roleId);
+		public void UpdateRoleByIdAsync(int roleId);
+	}
 }
--- PreOrderBlindBox/PreOrderBlindBox.Service/Services/RoleService.cs
+++ PreOrderBlindBox/PreOrderBlindBox.Service/Services/RoleService.cs
@@ -1,8 +1,35 @@
-﻿using PreOrderBlindBox.Services.IServices;
+﻿using PreOrderBlindBox.Data.Entities;
+using PreOrderBlindBox.Data.IRepositories;
+using PreOrderBlindBox.Services.IServices;
 
 namespace PreOrderBlindBox.Service.Services
-{ 
-    public class RoleService : IRoleService
-    {
-    }
+{
+	public class RoleService : IRoleService
+	{
+		private readonly IRoleRepository _roleRepository;
+
+		public RoleService(IRoleRepository roleRepository)
+        {
+            _roleRepository = roleRepository;
+        }
+        public void CreateRoleAsync(string roleName)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void DeleteRoleByIdAsync(int roleId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Role> GetRoleByIdAsync(int roleId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void UpdateRoleByIdAsync(int roleId)
+		{
+			throw new NotImplementedException();
+		}
+	}
 }
