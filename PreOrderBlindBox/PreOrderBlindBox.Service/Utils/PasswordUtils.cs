980fda0ff3776841ad1663e563fd9d6f94873027
NguyenDHung <hungndse171325@fpt.edu.vn>
2025-02-02 16:36:17 +0700

    Create wallet when confirm email, jwt in cookie

---
+++ PreOrderBlindBox/PreOrderBlindBox.Service/Utils/PasswordUtils.cs
@@ -0,0 +1,21 @@
+﻿using BCrypt.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PreOrderBlindBox.Services.Utils
+{
+	public class PasswordUtils
+	{
+		public static string HashPassword(string password)
+		{
+			return BCrypt.Net.BCrypt.EnhancedHashPassword(password, HashType.SHA256);
+		}
+		public static bool VerifyPassword(string password, string hashPassword)
+		{
+			return BCrypt.Net.BCrypt.EnhancedVerify(password, hashPassword, HashType.SHA256);
+		}
+	}
+}
