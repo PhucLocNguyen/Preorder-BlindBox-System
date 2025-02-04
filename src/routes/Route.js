1489dec3b1347eb1c914deb15c790bb73c83ab92
NguyenBaDat <bmbadat2234@gmail.com>
2025-02-04 10:30:50 +0700

    Complete order management

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -1,9 +1,15 @@
 import { Fragment, lazy } from "react";
+import { Navigate } from "react-router";
 const AdminLayout = lazy(() => import("../components/layouts/AdminLayout"));
 const DefaultLayout = lazy(() => import("../components/layouts/DefaultLayout"));
 const Home = lazy(() => import("../pages/Home/Home"));
 const LoginPage = lazy(() => import("../pages/Account/Login"));
 const Register = lazy(() => import("../pages/Account/Register"));
+const StaffView = lazy(() => import("../pages/Staff/StaffView"));
+const ProductsView = lazy(() => import("../pages/Staff/ProductsView"));
+const OrdersView = lazy(() => import("../pages/Staff/OrdersView"));
+const NotificationsView = lazy(() => import("../pages/Staff/NotificationView"));
+
 const publicRoutes = [
   {
     index: true,
@@ -13,6 +19,16 @@ const publicRoutes = [
   {
     path: "/login",
     component: LoginPage,
+  },
+  {
+    path: "/staff",
+    component: StaffView,
+    children: [
+      { index: true, component: ProductsView },
+      { path: "products", component: ProductsView },
+      { path: "orders", component: OrdersView },
+      { path: "notifications", component: NotificationsView },
+    ],
   },
   {
     path: "/account",
