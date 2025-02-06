64d636055a15a7a4c36ee3aefd7f3954bba32ce7
PhucLocNguyen <loc19992003bca@gmail.com>
2025-02-06 13:48:42 +0700

    Add cors config to connect reactjs and C# webapi

--- PreOrderBlindBox/PreOrderBlindBox.API/Program.cs
+++ PreOrderBlindBox/PreOrderBlindBox.API/Program.cs
@@ -11,103 +11,109 @@ using System.Text;
 
 namespace PreOrderBlindBox.Api
 {
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
-			var builder = WebApplication.CreateBuilder(args);
-			builder.Services.AddWebAPI();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddWebAPI();
 
-			builder.Services.AddDbContext<Preorder_BlindBoxContext>(options =>
-			{
-				options.UseSqlServer(builder.Configuration.GetConnectionString("MyDbContext"));
-			});
-			// Add services to the container.
-			builder.Services.AddControllers();
-			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-			builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddDbContext<Preorder_BlindBoxContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("MyDbContext"));
+            });
+            // Add services to the container.
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
 
-			builder.Services.AddSwaggerGen(c =>
-			{
-				c.SwaggerDoc("v1", new OpenApiInfo { Title = "Pre-order Blind Box Api", Version = "v1" });
-				c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-				{
-					Name = "Authorization",
-					Type = SecuritySchemeType.Http,
-					Scheme = "Bearer",
-					BearerFormat = "JWT",
-					In = ParameterLocation.Header,
-					Description = "Please enter a valid token",
-				});
-				c.AddSecurityRequirement(new OpenApiSecurityRequirement
-				{
-					{
-						new OpenApiSecurityScheme
-						{
-							Reference = new OpenApiReference
-							{
-								Type=ReferenceType.SecurityScheme,
-								Id="Bearer"
-							}
-						},
-						new string[]{}
-					}
-				});
-			});
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Pre-order Blind Box Api", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "Please enter a valid token",
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type=ReferenceType.SecurityScheme,
+                                Id="Bearer"
+                            }
+                        },
+                        new string[]{}
+                    }
+                });
+            });
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll",
+            builder => builder.WithOrigins("http://localhost:5173")
+                      .AllowAnyMethod()
+                      .AllowAnyHeader().AllowCredentials());
+            });
+            builder.Services.AddHttpContextAccessor();
 
-			builder.Services.AddHttpContextAccessor();
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies()
+                               .Where(a => !a.IsDynamic)
+                               .ToArray();
 
-			var assemblies = AppDomain.CurrentDomain.GetAssemblies()
-							   .Where(a => !a.IsDynamic)
-							   .ToArray();
+            builder.Services.AddAutoMapper(assemblies);
+            builder.Services.Configure<MailSettings>(builder.Configuration.GetSection("MailSettings"));
 
-			builder.Services.AddAutoMapper(assemblies);
-			builder.Services.Configure<MailSettings>(builder.Configuration.GetSection("MailSettings"));
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = builder.Configuration["JwtSettings:ValidIssuer"],
+                    ValidAudience = builder.Configuration["JwtSettings:ValidAudience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:SecretKey"]))
+                };
+            });
 
-			builder.Services.AddAuthentication(options =>
-			{
-				options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-				options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-				options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-			}).AddJwtBearer(options =>
-			{
-				options.RequireHttpsMetadata = false;
-				options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
-				{
-					ValidateIssuer = true,
-					ValidateAudience = true,
-					ValidateLifetime = true,
-					ValidateIssuerSigningKey = true,
-					ValidIssuer = builder.Configuration["JwtSettings:ValidIssuer"],
-					ValidAudience = builder.Configuration["JwtSettings:ValidAudience"],
-					IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:SecretKey"]))
-				};
-			});
+            //Background service cho voucher campaign
+            builder.Services.AddHostedService<VoucherCampaignBackgroundService>();
 
-			//Background service cho voucher campaign
-			builder.Services.AddHostedService<VoucherCampaignBackgroundService>();
+            var app = builder.Build();
 
-			var app = builder.Build();
-
-			// Configure the HTTP request pipeline.
-			if (app.Environment.IsDevelopment())
-			{
-				app.UseSwagger();
-				app.UseSwaggerUI(/*c =>
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(/*c =>
 				{
 					c.ConfigObject.AdditionalItems["persistAuthorization"] = true;
 				}*/);
-			}
+            }
 
-			app.UseHttpsRedirection();
-			app.UseMiddleware<JwtCookieMiddleware>();
+            app.UseHttpsRedirection();
+            app.UseMiddleware<JwtCookieMiddleware>();
 
-			app.UseAuthentication();
-			app.UseAuthorization();
+            app.UseAuthentication();
+            app.UseAuthorization();
 
-			app.MapControllers();
-
-			app.Run();
-		}
-	}
+            app.MapControllers();
+            app.UseCors("AllowAll");
+            app.Run();
+        }
+    }
 }
