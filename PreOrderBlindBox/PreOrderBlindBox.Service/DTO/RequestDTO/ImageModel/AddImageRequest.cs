5731e9694fe600b9c0f09fecdee236a733581667
Ha Gia Khanh <gkhanh903@gmail.com>
2025-02-06 21:51:29 +0700

    implement blob storage and image

---
+++ PreOrderBlindBox/PreOrderBlindBox.Service/DTO/RequestDTO/ImageModel/AddImageRequest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PreOrderBlindBox.Services.DTO.RequestDTO.ImageModel
+{
+    public class AddImageRequest
+    {
+        public List<IFormFile>? Files { get; set; }
+        public int? BlindBoxId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public List<bool?> IsMainImage { get; set; }
+    }
+}
