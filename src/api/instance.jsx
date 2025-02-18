664794121c0a8bae6e69af198d659b1100368eac
PhucLocNguyen <loc19992003bca@gmail.com>
2025-02-19 00:30:10 +0700

    Fix instance after merge

--- src/api/instance.jsx
+++ src/api/instance.jsx
@@ -3,6 +3,7 @@ import { GetAccessToken } from "./User/ApiAuthentication";
 
 
 const api = axios.create({
+   baseURL: 'https://preorderblindboxsystem-c9ftb6dtcvdkh3ge.centralus-01.azurewebsites.net/api'
 })
 api.interceptors.request.use(
    (config) => {
