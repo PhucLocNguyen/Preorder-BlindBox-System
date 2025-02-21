d93564f683467bcb2a9561dde3bcec919c9f30fa
hienmxse <hienmxse150606@fpt.edu.vn>
2025-02-21 11:03:01 +0700

    fix Homepage,Header

--- src/components/layouts/DefaultLayout.jsx
+++ src/components/layouts/DefaultLayout.jsx
@@ -1,9 +1,11 @@
 import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
 
 function DefaultLayout({ children }) {
 	return (
 		<div className='relative'>
 			<div>
+				<Header/>
 				<div className="bg-[url('//theme.hstatic.net/1000069970/1001119059/14/bg-vth.jpg?v=6844')]">
 					{children}
 				</div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,37 +1,13 @@
-import { Steps } from 'antd';
 import CountdownSection from '../../components/Sections/CountdownSection';
 import VoucherCampaignSection from '../../components/Sections/VoucherCampaignSection';
-import Navbar from '../../components/Navbar/Navbar';
 import ProductList from './ProductList/ProductList';
 import ProductHot from './ProductHot/ProductHot';
-import Header from '../../components/Header/Header';
-const description = 'This is a description.';
 
 function Home() {
 	return (
 		<div>
-			<Header />
-			<Navbar />
 			<ProductList />
-			<ProductHot />
-			<Steps
-				current={1}
-				items={[
-					{
-						title: 'Finished',
-						description,
-					},
-					{
-						title: 'In Progress',
-						description,
-						subTitle: 'Left 00:00:08',
-					},
-					{
-						title: 'Waiting',
-						description,
-					},
-				]}
-			/>
+			<ProductHot />	
 			<CountdownSection />
 			<VoucherCampaignSection />
 		</div>
