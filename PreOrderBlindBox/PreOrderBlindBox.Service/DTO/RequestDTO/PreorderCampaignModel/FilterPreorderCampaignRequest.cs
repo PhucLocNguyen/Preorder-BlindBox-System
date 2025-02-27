1a028c420ddce0e5b26d29ebca55f0dc6ce1f115
Ha Gia Khanh <gkhanh903@gmail.com>
2025-02-27 15:57:06 +0700

    Add filter campaign

---
+++ PreOrderBlindBox/PreOrderBlindBox.Service/DTO/RequestDTO/PreorderCampaignModel/FilterPreorderCampaignRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PreOrderBlindBox.Services.DTO.RequestDTO.PreorderCampaignModel
+{
+    public class FilterPreorderCampaignRequest
+    {
+        public bool isEndingSoon { get; set; }
+        public bool isNewlyLaunched { get; set; }
+        public bool isTrending {  get; set; }
+    }
+}
