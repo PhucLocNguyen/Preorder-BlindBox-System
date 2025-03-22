448aef3f6d21ddd1ba354af4e31fda4d94d267fb
PhucLocNguyen <loc19992003bca@gmail.com>
2025-03-22 11:59:08 +0700

    Add group for order hub to view live

--- PreOrderBlindBox/PreOrderBlindBox.Service/Hubs/OrderInCampaignHub.cs
+++ PreOrderBlindBox/PreOrderBlindBox.Service/Hubs/OrderInCampaignHub.cs
@@ -13,5 +13,22 @@ namespace PreOrderBlindBox.Services.Hubs
         {
             
         }
+        public async Task AddToGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+            await Clients.Group(groupName).SendAsync("ReceiveMessage", $"{Context.ConnectionId} đã tham gia nhóm {groupName}");
+        }
+
+        public async Task RemoveFromGroup(string groupName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+            await Clients.Group(groupName).SendAsync("ReceiveMessage", $"{Context.ConnectionId} đã rời khỏi nhóm {groupName}");
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", $"{Context.ConnectionId} đã ngắt kết nối.");
+            await base.OnDisconnectedAsync(exception);
+        }
     }
 }
